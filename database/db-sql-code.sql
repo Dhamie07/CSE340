916d1e4769743e8cc694c7c93506dd8cce932fc6
Dhamie07 <dokpewo@byupathway.edu>
2025-09-13 14:54:32 +0100

    Add SQL scripts for account management and inventory updates

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -6,7 +6,7 @@ CREATE TYPE public.account_type AS ENUM
     ('Client', 'Employee', 'Admin');
 
 ALTER TYPE public.account_type
-    OWNER TO cse340do;
+    OWNER TO db340;
 
 --Table structure for table `classification`
 CREATE TABLE public.classification(
@@ -239,3 +239,13 @@ VALUES   (
     'White',
     5
   );
+
+-- Sample Queries
+--4th Query
+UPDATE inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_make = 'GM' AND inv_model = 'Hummer';
+--6th Query
+UPDATE inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
