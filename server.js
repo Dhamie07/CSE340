b4c46f242899cec3ae27e5662796ca16706c84ea
Dhamie07 <dokpewo@byupathway.edu>
2025-09-15 13:19:11 +0100

    Implement inventory management features with classification view and navigation

--- server.js
+++ server.js
@@ -5,11 +5,13 @@
 /* ***********************
  * Require Statements
  *************************/
+const baseController = require("./controllers/baseController")
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const inventoryRoute = require("./routes/inventoryRoute")
 /* ***********************
  * View Engine and Templates
  *************************/
@@ -22,9 +24,9 @@ app.set("layout", "./layouts/layout")
  *************************/
 app.use(static)
 //Index route
-app.get("/", function(req, res) {
-  res.render("index", {title: "Home" })
-})
+app.get("/", baseController.buildHome)
+// Inventory routes
+app.use("/inv", inventoryRoute)
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
