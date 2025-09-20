76bfd0e8d79b50299579177912027d726773fba7
Dhamie07 <dokpewo@byupathway.edu>
2025-09-20 16:21:56 +0100

    Refactor server.js to set default values for port and host, ensuring server starts correctly.

--- server.js
+++ server.js
@@ -51,13 +51,13 @@ app.use(async (err, req, res, next) => {
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT || 3000;
+const host = '0.0.0.0';
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
-app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
+app.listen(port, host, () => {
+    console.log(`app listening on ${host}:${port}`);
+});
 
